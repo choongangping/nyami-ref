cf3f67b9637b815b454e7162cd30e133ee79b70a
jun <whdid3766@gmail.com>
2025-01-09 02:36:36 +0900

    mypage 홈 css 기본 구조 구현

--- frontend/src/pages/mypage/MyPage.jsx
+++ frontend/src/pages/mypage/MyPage.jsx
@@ -1,28 +1,54 @@
-import { Routes, Route, Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
+import { FaUser, FaCog, FaStar } from 'react-icons/fa';
+
+import styles from './MyPage.module.css';
 
 const MyPage = () => {
+  const location = useLocation();
+
   return (
-    <>
+    <div className={styles.mypage}>
       {/* Sidebar */}
-      <nav>
+      <nav className={styles.sidebar}>
         <ul>
-          <li>
-            <Link to="/mypage/activity">Activity</Link>
+          <li
+            className={
+              location.pathname === '/mypage/activity' ? styles.active : ''
+            }
+          >
+            <Link to="/mypage/activity">
+              <FaStar />
+              활동 내역
+            </Link>
           </li>
-          <li>
-            <Link to="/mypage/profile">Profile</Link>
+          <li
+            className={
+              location.pathname === '/mypage/profile' ? styles.active : ''
+            }
+          >
+            <Link to="/mypage/profile">
+              <FaUser />
+              프로필
+            </Link>
           </li>
-          <li>
-            <Link to="/mypage/account">Account</Link>
+          <li
+            className={
+              location.pathname === '/mypage/account' ? styles.active : ''
+            }
+          >
+            <Link to="/mypage/account">
+              <FaCog />
+              계정 정보
+            </Link>
           </li>
         </ul>
       </nav>
 
       {/* 하위 라우트 렌더링 */}
-      <div>
+      <div className={styles.content}>
         <Outlet />
       </div>
-    </>
+    </div>
   );
 };
 
