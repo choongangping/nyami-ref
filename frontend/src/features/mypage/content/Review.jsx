91a882bd2f74b252eea8b7ed1786f37fd3182be9
jun <whdid3766@gmail.com>
2025-01-09 22:14:17 +0900

    mypage 구조(디렉터리) 변경

---
+++ frontend/src/features/mypage/content/Review.jsx
@@ -0,0 +1,14 @@
+import styles from './Review.module.css';
+
+const Review = () => {
+  return (
+    <div className={styles.activity}>
+      {/* 리뷰 섹션 */}
+      <div className={styles.section}>
+        <h2>리뷰</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Review;
--- frontend/src/features/mypage/sidebar/Sidebar.jsx
+++ frontend/src/features/mypage/sidebar/Sidebar.jsx
@@ -4,9 +4,10 @@ import styles from './Sidebar.module.css';
 
 const Sidebar = () => {
   const menuItems = [
-    { link: '/mypage/activity', icon: FaStar, label: '활동 내역' },
     { link: '/mypage/profile', icon: FaUser, label: '프로필' },
     { link: '/mypage/account', icon: FaCog, label: '계정 정보' },
+    { link: '/mypage/like', icon: FaStar, label: '좋아요' },
+    { link: '/mypage/review', icon: FaStar, label: '리뷰' },
   ];
 
   return (
