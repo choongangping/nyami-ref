7186dc1fab7f7ba14689946ec26157267bd4157c
jun <whdid3766@gmail.com>
2025-01-09 18:34:13 +0900

    마이페이지 관련 폴더 구조 변경 , sidebar 컴포넌트화로 인한 css 이동

--- frontend/src/pages/mypage/MyPage.jsx
+++ frontend/src/pages/mypage/MyPage.jsx
@@ -1,49 +1,15 @@
-import { Outlet, Link, useLocation } from 'react-router-dom';
-import { FaUser, FaCog, FaStar } from 'react-icons/fa';
+import { Outlet } from 'react-router-dom';
 
 import styles from './MyPage.module.css';
+import Sidebar from '../../features/mypage/sidebar/Sidebar';
 
 const MyPage = () => {
-  const location = useLocation();
-
   return (
     <div className={styles.mypage}>
       {/* Sidebar */}
-      <nav className={styles.sidebar}>
-        <ul>
-          <li
-            className={
-              location.pathname === '/mypage/activity' ? styles.active : ''
-            }
-          >
-            <Link to="/mypage/activity">
-              <FaStar />
-              활동 내역
-            </Link>
-          </li>
-          <li
-            className={
-              location.pathname === '/mypage/profile' ? styles.active : ''
-            }
-          >
-            <Link to="/mypage/profile">
-              <FaUser />
-              프로필
-            </Link>
-          </li>
-          <li
-            className={
-              location.pathname === '/mypage/account' ? styles.active : ''
-            }
-          >
-            <Link to="/mypage/account">
-              <FaCog />
-              계정 정보
-            </Link>
-          </li>
-        </ul>
-      </nav>
-
+      <div className={styles.sidebar}>
+        <Sidebar />
+      </div>
       {/* 하위 라우트 렌더링 */}
       <div className={styles.content}>
         <Outlet />
