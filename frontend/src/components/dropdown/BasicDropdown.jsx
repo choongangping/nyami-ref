1093667ebce8fad3c9eacd7bdbc01221b7e37485
BY-ee <by-ee@naver.com>
2025-01-13 00:09:59 +0900

    ✨ feat[dropdown]: 외부, 내부 요소 클릭 시 패널 닫기 액션 추가

--- frontend/src/components/dropdown/BasicDropdown.jsx
+++ frontend/src/components/dropdown/BasicDropdown.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import styles from './BasicDropdown.module.css';
 
 /**
@@ -9,11 +10,35 @@ import styles from './BasicDropdown.module.css';
  * @returns {JSX.Element} 드롭다운 컴포넌트
  */
 const BasicDropdown = ({ children, isOpen, onClose }) => {
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    // 1. 패널 외부 클릭 시 드롭다운 패널을 닫는 함수 정의
+    const handleDropdownClose = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    // 2. isOpen 상태가 true일 때 이벤트 리스너 등록
+    if (isOpen) {
+      document.addEventListener('mousedown', handleDropdownClose);
+    }
+
+    // 3. 컴포넌트 언마운트 또는 isOpen이 false로 변경될 때 (클린업) 이벤트 해제
+    return () => {
+      document.removeEventListener('mousedown', handleDropdownClose);
+    };
+  }, [isOpen, onClose]);
+
   if (!isOpen) return null;
+
   return (
-    <div className={styles.dropdownContainer}>
+    <div className={styles.dropdownContainer} ref={dropdownRef}>
       <div className={styles.dropdown}>
-        <div className={styles.dropdownContent}>{children}</div>
+        <div className={styles.dropdownContent} onClick={onClose}>
+          {children}
+        </div>
       </div>
     </div>
   );
