db145b40f791d44f5e781eab6570d4a8aea7e6d3
BY-ee <by-ee@naver.com>
2025-01-28 23:37:20 +0900

    ✨ feat: 가게 조회 테스트 코드 작성
    - 테스트 케이스를 통과하는 최소한의 테스트 코드 구현

--- backend/src/main/java/com/project/store/controller/StoreController.java
+++ backend/src/main/java/com/project/store/controller/StoreController.java
@@ -4,13 +4,16 @@ import com.project.store.dto.StoreRequest;
 import com.project.store.dto.StoreResponse;
 import com.project.store.service.StoreService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @Slf4j
@@ -18,20 +21,27 @@ import java.util.List;
 @RequestMapping("/api")
 @Tag(name = "가게 API", description = "가게 API 명세서")
 public class StoreController {
+    private final StoreService storeService;
+
     @Operation(
         summary = "가게 목록",
-        description = "가게 목록을 쿼리 파라미터에 따라 페이징 처리 후 반환합니다."
+        description = "가게 목록을 쿼리 파라미터에 따라 페이징 처리 후 반환합니다.",
+        responses = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "가게 목록 조회 성공",
+                content = @Content(mediaType = "application/json", schema = @Schema(type = "array", implementation = StoreResponse.class))
+            ),
+            @ApiResponse(
+                responseCode = "500",
+                description = "가게 목록 조회 실패",
+                content = @Content(mediaType = "application/json", schema = @Schema(type = "string", example = "서버에 문제가 발생하였습니다."))
+            )
+        }
     )
     @GetMapping("/stores")
-    public List<StoreResponse> getStores() {
-        List<StoreResponse> storeResponses = new ArrayList<>();
-        StoreResponse storeResponse1 = new StoreResponse();
-        StoreResponse storeResponse2 = new StoreResponse();
-
-        storeResponses.add(storeResponse1);
-        storeResponses.add(storeResponse2);
-
-        return storeResponses;
+    public ResponseEntity<?> getStores() {
+        return ResponseEntity.ok(new ArrayList<StoreResponse>());
     }
 
     @Operation(
---
+++ backend/src/test/java/com/project/store/StoreServiceTest.java
@@ -0,0 +1,112 @@
+package com.project.store;
+
+import com.project.store.entity.FoodCategory;
+import com.project.store.entity.Local;
+import com.project.store.entity.Store;
+import com.project.store.entity.Theme;
+import com.project.store.service.StoreService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class StoreServiceTest {
+    @InjectMocks
+    private StoreService storeService;
+
+    private List<Store> mockStores;
+
+    @BeforeEach
+    void setUp() {
+        Local gangnam = new Local(1, "강남구");
+        Local seocho = new Local(2, "서초구");
+        FoodCategory korean = new FoodCategory(1, "한식");
+        FoodCategory chinese = new FoodCategory(2, "중식");
+        Theme soloTheme = new Theme(1, "혼밥하기 좋은");
+        Theme dateTheme = new Theme(2, "데이트 코스");
+
+        // Given
+        mockStores = Arrays.asList(
+                new Store(1, gangnam, korean, dateTheme, "Store A", "Address A", "Detail Address A", "010-1234-5678", "image1.png", new BigDecimal("100.000"), new BigDecimal("200.000"), "Description A", 100),
+                new Store(2, gangnam, chinese, soloTheme, "Store B", "Address B", "Detail Address B", "010-8765-4321", "image2.png", new BigDecimal("300.000"), new BigDecimal("400.000"), "Description B", 300),
+                new Store(3, seocho, chinese, dateTheme, "Store C", "Address C", "Detail Address C", "010-5678-1234", "image3.png", new BigDecimal("500.000"), new BigDecimal("600.000"), "Description C", 400),
+                new Store(4, seocho, korean, soloTheme, "Store D", "Address D", "Detail Address D", "010-4321-8765", "image4.png", new BigDecimal("700.000"), new BigDecimal("800.000"), "Description D", 200)
+        );
+    }
+
+    // 필터 조건 없음
+    @Test
+    @DisplayName("모든 가게 목록을 조회합니다.")
+    void getStores() {
+        // When
+        List<Store> stores = storeService.findStores(mockStores, null, null, null, null);
+
+        // Then
+        assertNotNull(stores);
+        assertEquals(4, stores.size());
+        assertEquals("Store C", stores.get(0).getName());
+    }
+
+    // 지역 필터 적용
+    @Test
+    @DisplayName("지역이 \"강남구\"인 가게 목록을 조회합니다.")
+    void getStoresByLocal() {
+        // When
+        List<Store> stores = storeService.findStores(mockStores, "강남구", null, null, null);
+
+        // Then
+        assertNotNull(stores);
+        assertEquals(2, stores.size());
+        assertEquals("강남구", stores.get(0).getLocal().getLocal());
+        assertEquals("강남구", stores.get(1).getLocal().getLocal());
+    }
+
+    // 지역 + 테마 필터 적용
+    @Test
+    @DisplayName("지역이 \"강남구\"이고 테마가 \"혼밥하기 좋은\"인 가게 목록을 조회합니다.")
+    void getStoresByLocalAndTheme() {
+        // When
+        List<Store> stores = storeService.findStores(mockStores, "강남구", null, "혼밥하기 좋은", null);
+
+        // Then
+        assertNotNull(stores);
+        assertEquals(1, stores.size());
+        assertEquals("강남구", stores.get(0).getLocal().getLocal());
+        assertEquals("혼밥하기 좋은", stores.get(0).getTheme().getTheme());
+    }
+
+    // 업종 필터 적용
+    @Test
+    @DisplayName("업종이 \"한식\"인 가게 목록을 조회합니다.")
+    void getStoresByFoodCategory() {
+        // When
+        List<Store> stores = storeService.findStores(mockStores, null, "한식", null, null);
+
+        // Then
+        assertNotNull(stores);
+        assertEquals(2, stores.size());
+        assertEquals("한식", stores.get(0).getFoodCategory().getFoodCategory());
+        assertEquals("한식", stores.get(1).getFoodCategory().getFoodCategory());
+    }
+
+    // 필터 조건 미적용 (잘못된 데이터)
+    @Test
+    @DisplayName("잘못된 데이터를 전달한 경우의 가게 목록을 조회합니다.")
+    void getStoresWithWrongData() {
+        // When
+        List<Store> stores = storeService.findStores(mockStores, "test", "test", "test", "test");
+
+        // Then
+        assertNotNull(stores);
+        assertTrue(stores.isEmpty());
+    }
+}
