12facba54b29a1d19ba4ccd72b40f5e68e0a44d7
BY-ee <by-ee@naver.com>
2025-01-28 23:50:35 +0900

    ✨ feat: 가게 조회 페이징 처리 구현

--- backend/src/main/java/com/project/store/service/StoreService.java
+++ backend/src/main/java/com/project/store/service/StoreService.java
@@ -15,12 +15,16 @@ import java.util.List;
 public class StoreService {
     private final StoreRepository storeRepository;
 
-    public List<Store> findStores(List<Store> stores, String local, String foodCategory, String theme, String sortBy) {
+    public List<Store> findStores(List<Store> stores, String local, String foodCategory, String theme, String sortBy, int page) {
+        int size = 6;
+
         return stores.stream()
                 .filter(store -> local == null || local.equals(store.getLocal().getLocal()))
                 .filter(store -> foodCategory == null || foodCategory.equals(store.getFoodCategory().getFoodCategory()))
                 .filter(store -> theme == null || theme.equals(store.getTheme().getTheme()))
                 .sorted(Comparator.comparingInt(Store::getViews).reversed())
+                .skip((long) (page - 1) * size)
+                .limit(size)
                 .toList();
     }
 }
