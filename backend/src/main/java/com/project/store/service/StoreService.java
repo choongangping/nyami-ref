29313c2d9526b8d468d729d57903871ac08cccb2
BY-ee <by-ee@naver.com>
2025-02-06 15:08:42 +0900

    🔧 fix: 불필요한 중복 예외처리 제거

--- backend/src/main/java/com/project/store/service/StoreService.java
+++ backend/src/main/java/com/project/store/service/StoreService.java
@@ -22,26 +22,17 @@ public class StoreService {
     int size = 6;
 
     public Page<StoreResponse> findStores(StoreSearchRequest request) {
-        try {
-            Pageable pageable = PageRequest.of(request.getPage(), size, Sort.by("views").descending());
-            Page<Store> stores = storeRepository.findStores(
-                    request.getLocal(),
-                    request.getFoodCategory(),
-                    request.getTheme(),
-                    request.getSort(),
-                    pageable
-            );
-            List<StoreResponse> responses = StoreMapper.STORE_MAPPER.toDto(stores.getContent());
+        Pageable pageable = PageRequest.of(request.getPage(), size, Sort.by("views").descending());
+        Page<Store> stores = storeRepository.findStores(
+                request.getLocal(),
+                request.getFoodCategory(),
+                request.getTheme(),
+                request.getSort(),
+                pageable
+        );
+        List<StoreResponse> responses = StoreMapper.STORE_MAPPER.toDto(stores.getContent());
 
-            return new PageImpl<>(responses, pageable, stores.getTotalElements());
-        } catch (IllegalArgumentException e) {
-            log.error("잘못된 파라미터 입력: local={}, foodCategory={}, theme={}, sort={}, page={}",
-                request.getLocal(), request.getFoodCategory(), request.getTheme(), request.getSort(), request.getPage(), e);
-            throw e;
-        } catch (Exception e) {
-            log.error("가게 조회 중 예외가 발생하였습니다:", e);
-            throw new RuntimeException("매장 검색 중 오류가 발생했습니다.", e);
-        }
+        return new PageImpl<>(responses, pageable, stores.getTotalElements());
     }
 
     public Optional<StoreWithMenuDto> findStoreById(int storeId) {
